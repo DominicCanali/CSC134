2f23b4c7eadb71bb963b611fd33cf128ed4d7d87
DominicCanali <156948014+DominicCanali@users.noreply.github.com>
2024-01-17 19:37:24 +0000

    M1T1 Done

---
+++ M1T1 Canali.cpp
@@ -0,0 +1,24 @@
+// Comment
+/*
+CSC 134
+M1T1 - Hello World
+D Canali
+1/17/24
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    string pokemon = "Giratina";
+    cout << "My favorite pokemon is " << pokemon << endl;
+    cout << "\n" << endl;
+    cout << "What's yours? ";
+    string answer;
+    cin >> answer;
+    cout << "You said " << answer << endl;
+    cout << "I guess that's ok." << endl << endl;
+    return 0;
+
+    return 0;
+}
